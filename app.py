34e4bdea1fcdb06ca305e90158855d9aa3765cf5
hanifa <hanifa.email@gmail.com>
2023-05-08 17:40:26 +0800

    classify UNSPSC, working

--- app.py
+++ app.py
@@ -1,29 +1,55 @@
 import streamlit as st
 import pandas as pd
 import os
-from myUtil import MyUtil
 import openai
-
+from st_aggrid import AgGrid
+from classifydata import ClassifyData
 from apikey import apikey
+
 os.environ['OPENAI_API_KEY']=apikey
 openai.api_key  = os.getenv('OPENAI_API_KEY')
+st.set_page_config(layout="wide")
 
-st.title('Generic Classsifcation using LLMs')
+def UNSPSC_dataset(df,sample_size=2):
+    '''Harcoded 
+    '''
+    sample_df=df.sample(sample_size)
+    X_df=sample_df[sample_df.columns[0:3]].reset_index()
+    Y_df=sample_df[sample_df.columns[3:]].reset_index()
+
+    X=X_df.to_dict(orient='records')
+    X=str(X).replace('{','{{',).replace('}','}}')
+            
+    return (X,X_df,Y_df)
+
+
+def sendServer(df):
+    input=UNSPSC_dataset(df)
+    st.write(input[1])
+
+
+    result=ClassifyData.classify_UNSPSC(input[0])
+    st.divider()
+
+    st.text('After classifying')
+    merged_df=pd.merge(input[1],result,on=['index','Product Description'],how='inner')
+    st.write(merged_df)
+
+
+st.title("LLM Usage (ChatGPT)")
+
+tab1,tab2=st.tabs(["Classification'","Others"])
+with tab1:
+    st.header("Generic Classsifcation using LLMs")
+
+    spectra = st.file_uploader("upload file", type={"csv", "txt"},)
+    if spectra is not None:
+        df = pd.read_csv(spectra)
+        sendServer(df)
+
+with tab2 :
+    st.image("https://media.tenor.com/kbbJOE4dIJkAAAAC/working-on.gif", width=400)
 
 
 
-spectra = st.file_uploader("upload file", type={"csv", "txt"},)
-if spectra is not None:
-    df = pd.read_csv(spectra)
-else:
-    df = pd.DataFrame({'A':[]})
 
-# st.write(df)
-
-input=MyUtil.get_tabular_UNSPSC(df)
-st.write(input[1])
-
-result=MyUtil.classify_UNSPSC(input[0])
-
-st.text('After classifying')
-st.write(result)
---
+++ classifydata.py
@@ -0,0 +1,69 @@
+import openai
+import pandas as pd
+import json
+
+class ClassifyData:
+    @staticmethod
+    def get_completion(prompt, model="gpt-3.5-turbo"):
+        messages = [{'role':'system', 'content':'You are an expert in classifying products into UNSPSC codes and speaks only JSON.Do not write normal text.'},
+                    {"role": "user", "content": prompt}]
+        
+        response = openai.ChatCompletion.create(
+            model=model,
+            messages=messages,
+            temperature=0, # this is the degree of randomness of the model's output
+        )
+        return response.choices[0].message["content"]
+
+    @staticmethod
+    def classify_UNSPSC(tabular_data):
+        prompt=f"""
+        You must classify the products delimited by triple backticks:
+        1.Provide the output with the following keys only
+        {{
+            "index":,
+            "Product Description": ,
+            "UNSPSC coding":,
+            "Segment": ,
+            "Family": ,
+            "Class": ,
+            "Commodity": 
+        }}
+        2.No AI introduction, No AI analysis, Return generated Json data only without backticks, Not human-readable, No backticks in output
+        3.If not sure, please at least guess the UNSPSC coding. 
+        ```{tabular_data}```"""
+        print(prompt)
+        response=ClassifyData.get_completion(prompt)
+        print(f'response:\t{response}')
+        response_dict=json.loads(response)
+        print(f'response_dict:\t{response_dict}')
+        
+        return pd.DataFrame.from_dict(response_dict)  
+
+
+    @staticmethod
+    def __process_tabular_UNSPSC(X_df,Y_df=None,sample_size=5):
+        X_df=X_df.sample(sample_size)
+        if Y_df:
+            Y_df=Y_df.filter(items=X_df.index)
+
+        X=X_df.to_dict(orient='records')
+        X=str(X).replace('{','{{',).replace('}','}}')
+
+        return X
+            
+
+    # @staticmethod
+    # def get_tabular_UNSPSC(df,sample_size=5):
+    #     sample_df=df.sample(sample_size)
+    #     print(sample_df.index)
+    #     sample_df=sample_df.reset_index(drop=True)
+    #     sample_df.rename(columns={'MaterialDescription': 'Product Description', 'Genericname ': 'Generic Product Description', 'SupplierName': 'Vendor Name'},inplace=True)
+    #     X_df=sample_df[sample_df.columns[0:3]].reset_index()
+    #     Y_df=sample_df[sample_df.columns[3:]].reset_index()
+
+    #     X=X_df.to_dict(orient='records')
+    #     X=str(X).replace('{','{{',).replace('}','}}')
+        
+    #     return (X,X_df,Y_df)
+    
